b19ee276d14258f7406f4617fe4cc95cc53986b0
wrlakshan <rashmika.lakshan@ebuilder.com>
2024-08-06 18:55:05 +0530

    add to the github

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
 # Welcome to your CDK TypeScript Construct Library project
 
-You should explore the contents of this project. It demonstrates a CDK Construct Library that includes a construct (`CdkS3CdnConstruct`)
-which contains an Amazon SQS queue that is subscribed to an Amazon SNS topic.
+You should explore the contents of this project. It demonstrates a CDK Construct Library that includes a construct (`CdkS3CdnConstruct`) which contains an Amazon SQS queue that is subscribed to an Amazon SNS topic.
 
 The construct defines an interface (`CdkS3CdnConstructProps`) to configure the visibility timeout of the queue.
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
+* `npm run build`   compile TypeScript to JavaScript
 * `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
+* `npm run test`    run the Jest unit tests
+* `tsc`             compile TypeScript to JavaScript (same as `npm run build`)
+
+## Setting up and Publishing the Package
+
+1. **Initialize the project with a scope**:
+   ```sh
+   npm init --scope=@wrlakshan
+   ```
+
+2. **Log in to npm**:
+   ```sh
+   npm login
+   ```
+
+3. **Publish the package with public access**:
+   ```sh
+   npm publish --access public
+   ```
+
+4. **Update the package version**:
+   ```sh
+   npm version 0.4.0
+   ```
+
--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,58 @@
-// import * as cdk from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export interface CdkS3CdnConstructProps {
-  // Define construct properties here
+  projectName: string,
+  s3WebSiteDeploySource: string,
+  websiteIndexDocuments: string,
+  websiteErrorDocuments: string,
+  cdnWebsiteIndexDocuments: string,
+  cdnComment: string,
 }
+
+
 
 export class CdkS3CdnConstruct extends Construct {
 
-  constructor(scope: Construct, id: string, props: CdkS3CdnConstructProps = {}) {
+  constructor(scope: Construct, id: string, props: CdkS3CdnConstructProps) {
     super(scope, id);
 
-    // Define construct contents here
+    const websiteBucket = new s3.Bucket(this, 'websiteBucket', {
+      bucketName: `${props.projectName}-${Math.floor(Math.random() * 1000000) + 1}`,
+      removalPolicy: RemovalPolicy.DESTROY,
+      websiteIndexDocument: props.websiteIndexDocuments,
+      websiteErrorDocument: props.websiteErrorDocuments,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkS3CdnConstructQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    websiteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        resources: [`${websiteBucket.bucketArn}/*`],
+        actions: ['s3:Get*'],
+        principals: [new iam.AnyPrincipal()],
+      })
+    )
+
+    const s3WebsiteDeployment = new s3deploy.BucketDeployment(this, 'websiteDeployment', {
+      sources: [s3deploy.Source.asset(props.s3WebSiteDeploySource)],
+      destinationBucket: websiteBucket,
+    });
+
+    const assertCdn = new cloudfront.Distribution(this, 'AssertCdn', {
+      defaultRootObject: props.cdnWebsiteIndexDocuments,
+      comment: props.cdnComment,
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+      },
+    });
+
+    new cdk.CfnOutput(this, 'websiteBucketUrl', { value: websiteBucket.bucketWebsiteUrl });
+    new cdk.CfnOutput(this, 'cdnUrl', { value: assertCdn.distributionDomainName });
   }
 }
